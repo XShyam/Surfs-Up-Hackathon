299b01e96ea43abdcc7f7aabe5cd68265211bdf5
Shyam <75221087+XShyam@users.noreply.github.com>
2021-06-13 19:04:55 +0530

    Add files via upload

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,19 @@
+<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+		
+					<li><a href="#"><i class="fa fa-desktop"></i> Boats</a>
+					<ul>
+						<li><a href="create-room.php">Add a Boat</a></li>
+						<li><a href="manage-rooms.php">Manage Boats</a></li>
+					</ul>
+				</li>
+
+
+				<li><a href="manage-students.php"><i class="fa fa-users"></i>Manage Clients</a></li>
+				
+
+			
+		</nav>
